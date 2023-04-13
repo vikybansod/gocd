92726169be3fe9c5bca497df2c406dea70250c70
vikybansod <viky.bansod@thoughtworks.com>
2023-04-13 14:42:08 +0530

    clean up

--- vars/tf.groovy
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env groovy
-
-def tfinit(String name = 'human') {
-  echo "Hello, ${name}, from terraform init."
-}
-
-def tfplan(String name = 'human') {
-  echo "Hello, ${name}, from terraform inits."
-}
